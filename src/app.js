d1f6c3b242643a8dd72f7836d61578d489aa1a8a
iso8king <ridhofirdaus321@gmail.com>
2025-11-08 16:46:30 +0700

    entah

--- src/app.js
+++ src/app.js
@@ -1,14 +1,7 @@
-// import { logger } from "./application/logging.js";
-// import { web } from "./application/web.js";
-
-// web.listen(9999 , ()=>{
-//     logger.info("App Listen at port 9999");
-// });
-
 import { logger } from "./application/logging.js";
 import { web } from "./application/web.js";
 
-logger.info("App loaded");
+web.listen(9999 , ()=>{
+    logger.info("App Listen at port 9999");
+});
 
-
-export default web;
