5cf9eb3feb27108012a98249b66e58031638dfb8
iso8king <ridhofirdaus321@gmail.com>
2025-11-08 16:34:26 +0700

    npm

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
+// import { logger } from "./application/logging.js";
+// import { web } from "./application/web.js";
+
+// web.listen(9999 , ()=>{
+//     logger.info("App Listen at port 9999");
+// });
+
 import { logger } from "./application/logging.js";
 import { web } from "./application/web.js";
 
 logger.info("App loaded");
+
 
 export default web;
