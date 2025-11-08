70e22195ce78326a7e7c2822198c561dc11e6a19
iso8king <ridhofirdaus321@gmail.com>
2025-11-08 16:11:04 +0700

    g

--- src/application/web.js
+++ src/application/web.js
@@ -6,11 +6,13 @@ import { errorMiddleware } from "../middleware/error-middleware.js";
 import { userRouter } from "../routes/api.js";
 import { swaggerUi } from "./swagger.js";
 import fs from "fs"
+import cors from "cors"
 
 dotenv.config();
 export const web = express();
 const swaggerDocument = JSON.parse(fs.readFileSync("./src/docs/swagger.json", "utf-8"));
 
+web.use(cors());
 web.use(express.json());
 web.use(cookieParser());
 web.use("/api/docs" , swaggerUi.serve , swaggerUi.setup(swaggerDocument));
