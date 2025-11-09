6ab403f04a0c62a34941af37f09e1659a0321e18
hisyam <hisyam.s901@gmail.com>
2025-11-09 16:42:57 +0700

    fix auth middleware

--- src/middleware/auth-middleware.js
+++ src/middleware/auth-middleware.js
@@ -1,32 +1,40 @@
-import jwt from "jsonwebtoken"
-import { responseError } from "../error/response-error.js"
+import jwt from "jsonwebtoken";
+import { responseError } from "../error/response-error.js";
 import { prismaClient } from "../application/database.js";
 
-export const authMiddleware = async(req,res,next)=>{
-   
-    const token = req.cookies.accessToken;
-    if(!token){
-        res.status(501).json({
-            errors : "Unauthorized"
-        });
-    }
-    else{
-        const tokenDecrypt = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET);
+export const authMiddleware = async (req, res, next) => {
+  const authorization = req.headers.authorization;
 
-        const user = await prismaClient.user.findFirst({
-        where : {
-            id : tokenDecrypt.id
-        }
-         });
-        
-         if(!user || user.status === "not_verified"){
-            res.status(501).json({
-            errors : "Unauthorized"
-        }).end();
-         }else{
-            req.user = user;
-            next();
-         }
+  if (!authorization) {
+    return res.status(403).json({
+      status: 403,
+      data: null,
+    });
+  }
 
-    }
-}
+  const [prefix, token] = authorization.split(" ");
+
+  if (!(prefix === "Bearer" && token)) {
+    return res.status(403).json({
+      status: 403,
+      data: null,
+    });
+  }
+
+  const tokenDecrypt = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  if (!tokenDecrypt) {
+    return res.status(403).json({
+      status: 403,
+      data: null,
+    });
+  }
+
+  const user = await prismaClient.user.findFirst({
+    where: {
+      id: tokenDecrypt.id,
+    },
+  });
+
+  req.user = user;
+  next();
+};
