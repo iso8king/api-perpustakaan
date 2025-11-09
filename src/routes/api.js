046fabd5e2b8913c67864752c8e9bb79e17b374a
hisyam <hisyam.s901@gmail.com>
2025-11-09 23:18:01 +0700

    fix api auth middleware

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,16 +1,11 @@
-import express from "express"
+import express from "express";
 import { authMiddleware } from "../middleware/auth-middleware.js";
 import userController from "../controller/user-controller.js";
 
-
 const userRouter = express.Router();
-userRouter.use(authMiddleware);
 
-userRouter.delete('/api/users/logout' , userController.logout);
+userRouter.delete("/api/users/logout", [authMiddleware], userController.logout);
+userRouter.get("/api/users/current", [authMiddleware], userController.get);
+userRouter.patch("/api/users/update", [authMiddleware], userController.updateProfile);
 
-userRouter.get('/api/users/current' , userController.get);
-userRouter.patch('/api/users/update' , userController.updateProfile)
-
-export {
-    userRouter
-}
+export { userRouter };
