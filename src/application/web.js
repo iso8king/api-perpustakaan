5583a58039004fdc292776178f922535c5ab5810
iso8king <ridhofirdaus321@gmail.com>
2025-11-10 21:36:12 +0700

    swagger fix

--- src/application/web.js
+++ src/application/web.js
@@ -1,28 +1,44 @@
-import express from "express"
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
-import { publicRouter } from "../routes/public-api.js";
-import { errorMiddleware } from "../middleware/error-middleware.js";
-import { userRouter } from "../routes/api.js";
-import { swaggerUi } from "./swagger.js";
-import fs from "fs"
-import cors from "cors"
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+
+import { publicRouter } from "../routes/public-api.js";
+import { userRouter } from "../routes/api.js";
+import { errorMiddleware } from "../middleware/error-middleware.js";
+import { swaggerUi } from "./swagger.js";
 
 dotenv.config();
 export const web = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const swaggerPath = path.join(__dirname, "../docs/swagger.json");
-const swaggerDocument = JSON.parse(fs.readFileSync(swaggerPath, "utf-8"));;
-
 web.use(cors());
 web.use(express.json());
 web.use(cookieParser());
-web.use("/api/docs" , swaggerUi.serve , swaggerUi.setup(swaggerDocument));
-web.use(publicRouter);
-web.use(userRouter)
 
-web.use(errorMiddleware)
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const swaggerPath = path.join(__dirname, "../docs/swagger.json");
+
+
+web.get("/api/docs/swagger.json", (req, res) => {
+  res.sendFile(swaggerPath);
+});
+
+
+web.use(
+  "/api/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/api/docs/swagger.json",
+    },
+  })
+);
+
+web.use(publicRouter);
+web.use(userRouter);
+web.use(errorMiddleware);
