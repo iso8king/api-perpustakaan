8e3534fa0d729a272f8cc1daff56146a3b93bf61
iso8king <ridhofirdaus321@gmail.com>
2025-11-10 21:08:09 +0700

    updateing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import { logger } from "./application/logging.js";
 import { web } from "./application/web.js";
 
-web.listen(9999 , ()=>{
-    logger.info("App Listen at port 9999");
-});
+logger.info("App loaded");
+
+
+export default web;
 
