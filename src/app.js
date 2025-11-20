0f68a6b68a37299d78d9705fa1cf234e7e286a44
iso8king <ridhofirdaus321@gmail.com>
2025-11-20 16:57:45 +0700

    minor

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import { logger } from "./application/logging.js";
 import { web } from "./application/web.js";
 
 logger.info("App loaded in port 9999");
-web.listen(9999)
+// web.listen(9999)
 
 export default web;
 
